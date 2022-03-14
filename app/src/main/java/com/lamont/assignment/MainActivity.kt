14db270c918fa29cf7f2362a2c03a2ac8aeef266
Lamont <lamontchean0517@gmail.com>
2022-03-14 23:19:00 +0800

    Initial commit

---
+++ app/src/main/java/com/lamont/assignment/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.lamont.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Bottom Navigation Bar
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navController = findNavController(R.id.main_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+    }
+}
