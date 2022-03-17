47087d3d1d6887503dcfa930b88ccbaa57f6918f
Jessie Liew <liwejss@gmail.com>
2022-03-17 22:41:24 +0800

    Jess 17/3 login register bottom nav bar

--- app/src/main/java/com/lamont/assignment/MainActivity.kt
+++ app/src/main/java/com/lamont/assignment/MainActivity.kt
@@ -2,10 +2,18 @@ package com.lamont.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Bottom Navigation Bar
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.loginRegister)
+        val navController = findNavController(R.id.account_fragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
