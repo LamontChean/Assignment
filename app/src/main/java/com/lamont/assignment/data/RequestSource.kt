4680922f5e3c9dd165fed7b9e7255ab0d822d629
Lamont <lamontchean0517@gmail.com>
2022-03-20 20:57:47 +0800

    CK, fixed layout 20/3

--- app/src/main/java/com/lamont/assignment/data/RequestSource.kt
+++ app/src/main/java/com/lamont/assignment/data/RequestSource.kt
@@ -6,7 +6,8 @@ class RequestSource {
     fun loadRequests(): List<Request>{
 
         return listOf<Request>(
-            Request("KaeLun", "I need an Ipad 9th for education purpose.", "Education")
+            Request("KaeLun", "I need an Ipad 9th for education purpose.", "Education"),
+            Request("Jessie", "I need an Iphone 13ProMax for education purpose. HahahahahaHahahahahaHahahahahaHahahahahaI need an Iphone 13ProMax for education purpose. HahahahahaHahahahahaHahahahahaHahahahaha", "Education")
         )
     }
 }
